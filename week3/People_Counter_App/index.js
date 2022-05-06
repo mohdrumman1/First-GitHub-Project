91328b9618c76cd11dc360cc3931e7fcfdfed3f4
Rumman <mohdrumman1@gmail.com>
2022-05-06 17:18:50 +1000

    user

--- week3/People_Counter_App/index.js
+++ week3/People_Counter_App/index.js
@@ -2,6 +2,12 @@
 
 let countEl = document.getElementById("count-el")
 let count = 0
+let username = "Rumman"
+let greeting = "Welcome back "
+let messageToUser = message + ", " + username + "!"
+let welcomeEl = document.getElementById("welcome-el")
+
+welcomeEl.innerText = greeting + username
 
 function increment(){
     count = count + 1
