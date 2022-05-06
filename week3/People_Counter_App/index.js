aaae1ef159bd249d63a96321883530f618b5de5b
Rumman <mohdrumman1@gmail.com>
2022-05-06 17:34:08 +1000

    save function

--- week3/People_Counter_App/index.js
+++ week3/People_Counter_App/index.js
@@ -1,21 +1,16 @@
 //document.getElementById("count-el").innerText = increment
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
-let username = "Rumman"
-let greeting = "Welcome back "
-let messageToUser = message + ", " + username + "!"
-let welcomeEl = document.getElementById("welcome-el")
-
-welcomeEl.innerText = greeting + username
 
 function increment(){
-    count = count + 1
+    count += 1
     countEl.innerText = count
-    console.log(count)
 }
  
 
 function save(){
-    console.log(count)
+    dash = count + " - "
+    saveEl.innerText += dash
 }
