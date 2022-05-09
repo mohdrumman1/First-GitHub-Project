a9a3db1e437b36a3b53c5dd2f09737beee21591a
Rumman <mohdrumman1@gmail.com>
2022-05-09 17:15:31 +1000

    boolean

--- week3/BlackJack_App/index.js
+++ week3/BlackJack_App/index.js
@@ -2,13 +2,19 @@
 let firstCard = (Math.random() * 11) + 2
 let secondCard = (Math.random() * 11) + 2
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
 if(sum <= 20){
     console.log("Do you want to draw a new card?")
 }
 else if(sum === 21){
     console.log("Congratulations! You've won!")
+    hasBlackJack = true
 }
 else{
     console.log("You're out of the game :( ")
+    isAlive = false
 }
+
+ console.log(hasBlackJack)
