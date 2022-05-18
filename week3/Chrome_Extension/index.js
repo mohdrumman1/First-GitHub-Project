e416cd03fc9a823123a6a7f8f91a085e6619e755
Rumman <mohdrumman1@gmail.com>
2022-05-18 19:43:26 +1000

    template

--- week3/Chrome_Extension/index.js
+++ week3/Chrome_Extension/index.js
@@ -1,18 +1,25 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
+    renderLeads()
 })
 
-let listItems = ""
-
-for(let i = 0; i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
+function renderLeads(){
+    let listItems = ""
+    for(let i = 0; i < myLeads.length; i++){
+        //listItems += "<li><a href='" + myLeads[i] + "' target = '_blank'>" + myLeads[i]  + "</a></li>"
+        listItems += `
+                    <li>
+                        <a href='${myLeads[i]}' target = '_blank'>${myLeads[i]}</a>
+                    </li>
+                    `
+    }
+    ulEl.innerHTML = listItems
 }
 
-ulEl.innerHTML = listItems
 
