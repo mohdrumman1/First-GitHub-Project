e8da497e61e48c90cf82067c8638d7364b543e5b
Rumman <mohdrumman1@gmail.com>
2022-05-31 17:30:49 +1000

    check

--- week3/Chrome_Extension/index.js
+++ week3/Chrome_Extension/index.js
@@ -2,11 +2,17 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+localStorage.clear()
+let leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
+
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads(){
