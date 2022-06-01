17834bea194ba6ce37620c5367382fe9bad94097
Rumman <mohdrumman1@gmail.com>
2022-06-01 18:59:22 +1000

    get current tab

--- week3/Chrome_Extension/index.js
+++ week3/Chrome_Extension/index.js
@@ -12,14 +12,14 @@ if(leadsFromStorage){
     render(myLeads)
 }
 
-const tabs = [
-    {url: "https://www.linkedin.com/in/mohammed-rumman-riyaz-4b8576163/"}
-]
-
 tabBtn.addEventListener("click",function(){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    render(myLeads)
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){  
+    // })
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
 function render(leads){
